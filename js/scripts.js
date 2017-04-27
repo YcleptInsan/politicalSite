6e2c39c54d8d8a375416164c175fc6eff3624b52
nick wise <nickmwise@gmail.com>
2017-04-27 16:39:10 -0700

    Work on front end logic to return result. not final

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
+
+
+
 $(document).ready(function(){
 	$("form#all").submit(function(event){
 		event.preventDefault();
-		var pickNum = $("input#Q1").val()
-			if (pickNum <= 100) {
-				show("#Q1-text-below");
-				alert("do this here")
-		// var favColor = $("input#Q2").val();
-		// var marajaunaQuestion = $("input#Q3").val();
+
+		var pickNum = $("input#Q1").val();
+		 if (pickNum <= 100){
+
+		var favColor = $("input#Q2").val();
+		var marajaunaQuestion = $("input#Q3").val();
 		var result = pickNum;
-	}
-
-		alert(result);
-
-		$("#output").appendTo(result);
+		$("#output").text(result);
+	};
 	});
 });
